3ce8011cd0b5c68730fa1d14ed599f59ae043d3d
Komal Sharan <komalssharan@gmail.com>
2018-07-02 09:03:24 -0400

    my angular resume

---
+++ app.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app', ['ngRoute', 'ngCookies'])
+        .config(config)
+        .run(run);
+
+    config.$inject = ['$routeProvider', '$locationProvider'];
+    function config($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'resume.html',
+                controller:'ServicesCtrl',
+
+            })
+
+
+    }
+
+
+
+})();
